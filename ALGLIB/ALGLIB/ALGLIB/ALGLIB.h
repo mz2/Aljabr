a8d44f000e93c5ad0dd94aa7170e94a100b9415e
Matias Piipari <matias.piipari@gmail.com>
2016-05-12 16:31:13 +0100

    Adds ALGLIB CPP source + framework.

---
+++ ALGLIB/ALGLIB/ALGLIB/ALGLIB.h
@@ -0,0 +1,10 @@
+//
+//  ALGLIB.h
+//  ALGLIB
+//
+//  Created by Matias Piipari on 24/12/2013.
+//  Copyright (c) 2013 Matias Piipari. All rights reserved.
+//
+
+#import "ap.h"
+#import "dataanalysis.h"
